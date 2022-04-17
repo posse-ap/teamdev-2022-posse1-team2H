40afe207ce98a93a38944daa4ec3184aeb6e265a
fukuba-shuma <calculus.fukuba@gmail.com>
2022-04-18 08:20:51 +0900

    fixed env settings

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -6,35 +6,98 @@ USE shukatsu;
 
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+-- CREATE TABLE users (
+--   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--   email VARCHAR(255) UNIQUE NOT NULL,
+--   password VARCHAR(255) NOT NULL,
+--   tel VARChAR(255) NOT NULL,
+--   univercity VARCHAR(255) NOT NULL,
+--   undergraduage VARCHAR(255) NOT NULL,
+--   department VARCHAR(255) NOT NULL,
+--   school_year INT(10) NOT NULL,
+--   graduation_year INT(10) NOT NULL,
+--   gender BOOLEAN NOT NULL,
+--   address VARCHAR(255) NOT NULL,
+--   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+-- );
+
+-- INSERT INTO
+--   users
+-- SET
+--   email = 'test@posse-ap.com',
+--   password = sha1('password');
+
+DROP TABLE IF EXISTS agencies;
+
+CREATE TABLE agencies (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
+  email_for_notification VARCHAR(255) NOT NULL,
+  tel VARCHAR(255) NOT NULL,
+  url TEXT NOT NULL,
+  representative VARCHAR(255) NOT NULL,
+  contactor VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO
-  users
-SET
-  email = 'test@posse-ap.com',
-  password = sha1('password');
+DROP TABLE IF EXISTS users_agencies;
 
-DROP TABLE IF EXISTS events;
+CREATE TABLE users_agencies (
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  user_id INT,
+  agency_id INT
+);
 
-CREATE TABLE events (
+DROP TABLE IF EXISTS industries;
+
+CREATE TABLE industries (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
+  industry VARCHAR(255) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO
-  events
-SET
-  title = 'イベント1';
+DROP TABLE IF EXISTS agency_type;
 
-INSERT INTO
-  events
-SET
-  title = 'イベント2';
+CREATE TABLE agency_type (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  agency_typr VARCHAR(255) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS agencies_industories;
+
+CREATE TABLE agencies_industories (
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  agency_id INT,
+  industory_id INT
+);
+
+DROP TABLE IF EXISTS agencies_types;
+
+CREATE TABLE agencies_types (
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  agency_id INT,
+  type_id INT
+);
+
+DROP TABLE IF EXISTS managers;
+
+CREATE TABLE managers (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  is_representative BOOLEAN NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
