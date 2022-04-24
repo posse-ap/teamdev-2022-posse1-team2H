a1f8b6756f75a42d298cf3fb3cc3cc2ead50e25b
fukuba-shuma <calculus.fukuba@gmail.com>
2022-04-24 22:04:04 +0900

    getAgency

--- src/cruds/user.php
+++ src/cruds/user.php
@@ -63,4 +63,68 @@ class User
 
         return json_encode($values, JSON_UNESCAPED_UNICODE);
     }
+
+    public function getAgency($id) {
+        $stmt = $this->db->prepare('SELECT
+            agency.id,
+            agency.name,
+            agency.email,
+            agency.tel,
+            agency.url,
+            agency.representative,
+            agency.address,
+            agency.address_num,
+            article.title,
+            article.sentenses,
+            article.eyecatch_url
+            FROM agencies as agency
+            INNER JOIN agency_articles as article
+            ON agency.id = article.agency_id
+            WHERE agency.id = :id
+        ');
+        $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+        $stmt->execute();
+        $item = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        $type_stmt = $this->db->prepare('SELECT
+            agency_type
+            FROM agency_type
+            WHERE id IN (
+                SELECT type_id FROM agencies_types WHERE agency_id = :id
+            )
+        ');
+
+        $type_stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+        $type_stmt->execute();
+        $types = $type_stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        $industry_stmt = $this->db->prepare('SELECT
+            industry
+            FROM industries
+            WHERE id IN (
+                SELECT industry_id FROM agencies_industries WHERE agency_id = :id
+            )
+        ');
+        $industry_stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+        $industry_stmt->execute();
+        $industries = $industry_stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        extract($item);
+        $result = array(
+            'id' => $id,
+            'name' => $name,
+            'email' => $email,
+            'tel' => $tel,
+            'url' => $url,
+            'representative' => $representative,
+            'address' => $address,
+            'address_num' => $address_num,
+            'title' => $title,
+            'sentenses' => $sentenses,
+            'eyecatch_url' => $eyecatch_url,
+            'types' => $types,
+            'industries' => $industries
+        );
+        return json_encode($result, JSON_UNESCAPED_UNICODE);
+    }
 }
