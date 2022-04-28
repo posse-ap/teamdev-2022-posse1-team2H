4c8a64b1fe8d7fd4952393cd83b82973584d233b
加茂竜之介 <Kamoryu2001@gmail.com>
2022-04-28 09:13:09 +0900

    一旦コミット

--- src/header.php
+++ src/header.php
@@ -18,5 +18,6 @@
     <header class="header">
         <header class="header_inner">
             <h1 class="header_inner_craft">CRAFT</h1>
+            <h3 class="header_inner_catchCopy">学生のための就職情報サイト</h3>            
         </header>
     </header>
