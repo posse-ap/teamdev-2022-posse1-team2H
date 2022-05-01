0ddae52e0ff09487afc996659482af37decd4b74
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-01 20:33:59 +0900

    ほぼ完成

--- src/index.php
+++ src/index.php
@@ -31,41 +31,40 @@ include dirname(__FILE__) . "/header.php";
                             <div class="agency_infromation">
                                 <div class="name_and_favorite">
                                     <div class="agency_name_wrapper"><span class="agency_name">三菱ufj信託銀行</span></div>
-                                    <div id="star" class="star"><i class="fa-solid fa-star"></i></div>
+                                    <div id="star<?php echo $i ?>" class="star" onclick="changingColor(this)"><i class="fa-solid fa-star"></i></div>
 
                                 </div>
                                 <div class="agency_slogan_wrapper"><span class="slogan">面接対策,ES添削で内定サポート！ 就活エージェントおすすめ15選</span></div>
                             </div>
                             <div class="agency_tags_wrapper">
                                 <i class="fa-solid fa-tags" style="color: #9E9E9E"></i>
-                                <a href="">#広告</a>
-                                <a href="">#コンサル</a>
-                                <a href="">#文系に強い</a>
-                                <a href="">#ベンチャーが多い</a>
+                                <a href=""><span class="filling"></span>#広告</a> 
+                                <a href=""><span class="filling"></span>#コンサル</a>
+                                <a href=""><span class="filling"></span>#文系に強い</a>
+                                <a href=""><span class="filling"></span>#ベンチャーが多い</a>
                             </div>
                         </div>
 
                     </article>
                 <?php endfor ?>
             </div>
+        </div>  
 
-        </div>
         <div class="popular_agency_wrapper">
             <h1 class="title">人気</h1>
             <div class="popular_agency_inner">
-                <?php for ($i = 0; $i < 4; $i++) : ?>
+                <?php for ($i = 6; $i < 12; $i++) : ?>
                     <article class="popular_agency_card">
                         <div class="agency_img">
                             <a href="https://posse-ap.com/">
-                                <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="">
+                                <img src="https://www.ntt-f.co.jp/architect/shared/inpex-naoetsu/d-01.jpg" alt="">
                             </a>
                         </div>
                         <div class="agency_card_content">
                             <div class="agency_infromation">
                                 <div class="name_and_favorite">
-                                    <div class="agency_name_wrapper"><span class="agency_name">三菱UFJ銀行</span></div>
-                                    <div id="star" class="star"><i class="fa-solid fa-star"></i></div>
-
+                                    <div class="agency_name_wrapper"><span class="agency_name">国際石油開発帝石ホールディングス</span></div>
+                                    <div id="star<?php echo $i ?>" class="star" onclick="changingColor2(this)"><i class="fa-solid fa-star"></i></div>
                                 </div>
                                 <div class="agency_slogan_wrapper"><span class="slogan">面接対策,ES添削で内定サポート！ 就活エージェントおすすめ15選</span></div>
                             </div>
@@ -90,7 +89,7 @@ include dirname(__FILE__) . "/header.php";
         <aside id="sidebar_inner" class="sidebar_inner">
             <div class="sidebar_favorite_area">
                 <div class="sidebar_favorite">
-                    <h2 onclick="transtioning()">後で見る</h2>
+                    <h2 onclick="transitioning()">後で見る</h2>
                 </div>
                 <div class="favorite_content"></div>
             </div><br>
@@ -100,7 +99,7 @@ include dirname(__FILE__) . "/header.php";
                         <h2 onclick="appearSidebar()">検索</h2>
                         <div id="serach_content" class="serach_content">
                             <div id="business_type_wrapper" class="business_type_wrapper">
-                                <h3 onclick="appearIndustryTypes()">業種</h3>
+                                <h3 onclick="appearIndustryTypes()"><i class="fa-solid fa-building"></i>業種</h3>
                                 <div id="business_type_items" class="business_type_inner">
                                     <?php foreach ($industries as $industry) : ?>
                                         <label>
@@ -110,7 +109,7 @@ include dirname(__FILE__) . "/header.php";
                                 </div>
                             </div>
                             <div class="business_features_wrapper">
-                                <h3 onclick="appearTypes()">特徴</h3>
+                                <h3 onclick="appearTypes()"><i class="fa-solid fa-glasses"></i>特徴</h3>
                                 <div id="business_features" class="business_type_inner">
                                     <?php foreach ($types as $type) : ?>
                                         <label>
