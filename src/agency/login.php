fcf1c16218449c904ddeaee192a7fe1ca45eff24
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-03 17:51:34 +0900

    login exec

--- src/agency/login.php
+++ src/agency/login.php
@@ -3,7 +3,19 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 use modules\auth\Agency;
 
 $auth = new Agency($db);
-if ($_SERVER[''])
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+        $manager = $auth->login($_POST['email'], $_POST['password']);
+        if (!isset($_SESSION['agency']['id'])) {
+            $error['login'] = 'failed';
+        } else {
+            header('Location: index.php');
+        }
+    } else {
+        $error['login'] = 'blank';
+    }
+
+}
 
 
 include dirname(__FILE__) . '/header.php';
