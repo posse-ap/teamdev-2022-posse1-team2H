dc58fa210b7d1cceafe0de59b57319b6ee6e9c61
mirei1115 <wanima9990@gmail.com>
2022-05-11 23:44:40 +0900

    phpに移行

--- src/agency/index.php
+++ src/agency/index.php
@@ -5,18 +5,13 @@ use modules\auth\Agency;
 $auth = new Agency($db);
 $auth->validate();
 
-
 include dirname(__FILE__) . '/header.php' ?>
-<main class="content" id="agency_top">
-
 
 <div class="export-button"><a href="">エクスポート</a></div>
 <div class="private-big-box">
-    <p class="private-small-box"><a href="detail.html">2022/01/22 22:39 田中太郎 男 21歳 taro.tanaka@gmail.com</a></p>
+    <p class="private-small-box"><a href="detail.php">2022/01/22 22:39 田中太郎 男 21歳 taro.tanaka@gmail.com</a></p>
     <p class="private-small-box"><a href="">2022/01/22 21:35 山田桜 女 22歳 sakura.yamada@gmail.com</a></p>
     <p class="private-small-box"><a href="">2022/01/20 23:59 鈴木誠也 男 22歳 seiya.suzuki@gmail.com</a></p>
 </div>
-
-</main>
 
 <?php include dirname(__FILE__) . '/footer.php' ?>
