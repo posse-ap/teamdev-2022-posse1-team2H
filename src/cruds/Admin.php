dabca2323e7175434e619fd363f5ad4ef0f538a2
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-13 15:15:50 +0900

    call getAgencies method from js

--- src/cruds/Admin.php
+++ src/cruds/Admin.php
@@ -26,4 +26,49 @@ class Admin
         $stmt->execute();
         return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
+
+    public function getAgencies($sort = true)
+    {
+        if ($sort) {
+            $sort_mode = "DESC";
+        } else {
+            $sort_mode = "ASC";
+        }
+        $stmt = $this->db->prepare(sprintf("SELECT
+        agencies.id agency_id,
+        agencies.name agency_name,
+        contracts.claim_year_month claim,
+        COUNT(users_agencies.user_id) user_count
+        FROM agencies
+        LEFT JOIN contracts
+        ON agencies.id = contracts.agency_id
+        LEFT JOIN users_agencies
+        ON agencies.id = users_agencies.agency_id
+        WHERE users_agencies.user_id IN (
+            SELECT id FROM users
+            WHERE DATE_FORMAT(updated_at, '%Y%m') = DATE_FORMAT(now(), '%Y%m')
+        )
+        ORDER BY agencies.updated_at %s
+        ", $sort_mode));
+
+        $stmt->execute();
+
+        $num = $stmt->rowCount();
+
+        if ($num > 0) {
+            $values = array();
+            while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                extract($row);
+                $item = array(
+                    'id' => $id,
+                    'name' => $name,
+                    'claim' => $claim,
+                    'user_count' => $user_count,
+                );
+                array_push($values, $item);
+            }
+            return json_encode($values, JSON_UNESCAPED_UNICODE);
+        }
+        return json_encode(array());
+    }
 }
