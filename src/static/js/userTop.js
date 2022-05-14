a34e4395f5f8c63e329e07e2888fdf32f8e795f8
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-14 11:55:53 +0900

    ３度目の保存

--- src/static/js/userTop.js
+++ src/static/js/userTop.js
@@ -32,19 +32,11 @@ let newIdArray = [];
 const changingColor = (newColor) => {
     const specifiedChangingColor = (new_color,i) => {
         let new_color_id = document.getElementById(new_color);
-        if(new_color_id.className=='star'+[i]+''){
+        if (new_color_id.className == 'star' + [i] + '') {
             new_color_id.classList.add('changing_color');
-            if (slicedArray) {
-                slicedArray.push(idArray[i]);
-                Cookies.set('ids', `${slicedArray}`, { expires: 1 });
-                console.log(Cookies.get());
-            } else {
-                newIdArray.push(idArray[i]);
-                Cookies.remove('ids');
-                Cookies.set('ids', `${newIdArray}`, { expires: 1 });
-                console.log(Cookies.get());
-            }
-
+            newIdArray.push(idArray[i]);
+            Cookies.set('ids', `${newIdArray}`, { expires: 1 });
+            console.log(Cookies.get());
         } else {
             new_color_id.classList.remove('changing_color');
             Cookies.remove('ids');
