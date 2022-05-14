22a8ef147a735f8646f4e8bc3da35187652b4edb
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-14 13:31:50 +0900

    getaAgencies作りかけ

--- src/cruds/Admin.php
+++ src/cruds/Admin.php
@@ -4,9 +4,42 @@ namespace cruds;
 
 class Admin
 {
+    // 学生単価
+    private const STUDENT_UNIT_PRICE = 1000;
+
     public function __construct($db)
     {
         $this->db = $db;
+    }
+
+    public function createContract() {
+        $year_month = date("Y-m", strtotime('last month'));
+        $stmt = $this->db->prepare("SELECT agency_id, COUNT(*) users_count FROM users_agenciees WHERE user_id IN (
+            SELECT id FROM users
+            WHERE DATE_FORMAT(updated_at, '%Y%m') = :year_month
+        ) GROUP BY agency_id");
+        $stmt->bindValue(":year_month", $year_month, \PDO::PARAM_INT);
+        $stmt->execute();
+        $num = $stmt->rowCount();
+
+        if ($num > 0) {
+            while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                extract($row);
+                $price = self::STUDENT_UNIT_PRICE * $user_count;
+                $claim = date('Y-m-d', strtotime('last day of next month', $year_month));
+                $create_stmt = $this->db->prepare("INSERT INTO ccontracts
+                (agency_id, contract_year_month, claim_year_month, request_amounts)
+                VALUES
+                (:agency_id, :contract_year_month, :claim_year_month, :request_amounts)
+                ");
+                $create_stmt->bindValue(":agency_id", $agency_id, \PDO::PARAM_INT);
+                $create_stmt->bindValue(":contract_year_month", $year_month, \PDO::PARAM_INT);
+                $create_stmt->bindValue(":claim_year_month", $claim);
+                $create_stmt->bindValue(":request_amounts", $request_amounts, \PDO::PARAM_INT);
+                $create_stmt->execute();
+            }
+        }
+        return;
     }
 
     public function insertAdmins()
@@ -27,30 +60,26 @@ class Admin
         return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 
-    public function getAgencies($sort = true)
+    public function getAgencies($year, $month, $sort = true)
     {
+        $date_format = (string)$year . sprintf('%02d', $month);
         if ($sort) {
             $sort_mode = "DESC";
         } else {
             $sort_mode = "ASC";
         }
-        $stmt = $this->db->prepare(sprintf("SELECT
+        $query = "SELECT
         agencies.id agency_id,
         agencies.name agency_name,
         contracts.claim_year_month claim,
-        COUNT(users_agencies.user_id) user_count
+        contracts.request_amounts amounts
         FROM agencies
         LEFT JOIN contracts
         ON agencies.id = contracts.agency_id
         LEFT JOIN users_agencies
         ON agencies.id = users_agencies.agency_id
-        WHERE users_agencies.user_id IN (
-            SELECT id FROM users
-            WHERE DATE_FORMAT(updated_at, '%Y%m') = DATE_FORMAT(now(), '%Y%m')
-        )
-        ORDER BY agencies.updated_at %s
-        ", $sort_mode));
-
+        ORDER BY agencies.updated_at " . $sort_mode ."";
+        $stmt = $this->db->prepare($query);
         $stmt->execute();
 
         $num = $stmt->rowCount();
