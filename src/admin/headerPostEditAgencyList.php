a8b6c6d08ab6b4af9be2dec3793c1666ae92253f
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-15 22:18:05 +0900

    掲載・編集関連の企業一覧見た目完成

---
+++ src/admin/headerPostEditAgencyList.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/css/sanitize.css">
+    <link rel="stylesheet" href="../static/css/adminPostEditAgencyList.css">
+    <script src="https://kit.fontawesome.com/727d59e43e.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <title>Craft for Boozer</title>
+</head>
+<body>
+    <header>
+    <div class="header_inner">
+            <div class="page_name">管理画面</div>
+        </div>
+    </header>
