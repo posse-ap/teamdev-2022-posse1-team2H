da7302cedfa3be18a68a4003193b79b9c10035c4
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-15 09:38:39 +0900

    保存

--- src/admin/agencyInfo.php
+++ src/admin/agencyInfo.php
@@ -16,6 +16,34 @@ include dirname(__FILE__) . '/headerAgencyInfo.php';
         <div class="got_information">総情報獲得数：6件</div>
         <div class="total_amount">合計金額：2000</div>
     </div>
+    <div class="agency_list_wrapper">
+        <!-- label forとinputのidが同じ値である必要があります -->
+        <?php for ($i = 1; $i < 4; $i++) : ?>
+            <!-- 仮で、支払い済みと未払いで分けています。どちらか、消してもらって大丈夫です。 -->
+            <ul class="agency_list_inner">
+                <ol>
+                    <a href="./agencyInfo.php">○○○○llllllllllllll株式会社</a>
+                    <div>情報獲得数　2件</div>
+                    <div>期限　4/30</div>
+                    <div>金額xxxx</div>
+                    <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden"></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>">支払い済み</label>
+                </ol>
+
+            </ul>
+        <?php endfor ?>
+        <?php for ($i = 4; $i < 7; $i++) : ?>
+            <ul class="agency_list_inner">
+                <ol>
+                    <a href="./agencyInfo.php">○○○○llllllllllllll株式会社</a>
+                    <div>情報獲得数　2件</div>
+                    <div>期限　4/30</div>
+                    <div>金額xxxx</div>
+                    <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden"></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>">未払い</label>
+                </ol>
+
+            </ul>
+        <?php endfor ?>
+    </div>
 </main>
 
 <?php include dirname(__FILE__) . '/footerAgencyInfo.php' ?>
