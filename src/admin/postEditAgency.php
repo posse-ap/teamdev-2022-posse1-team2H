b89c0a36886047f52d59ae4946d89b3942089101
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-16 00:18:15 +0900

    実際に編集したりする、掲載したりする画面以外は完了

---
+++ src/admin/postEditAgency.php
@@ -0,0 +1,14 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+
+include dirname(__FILE__) . '/headerPostEditAgency.php';
+?>
+
+<main>
+    <div id="displayed_content" class="displayed_content">○○○○株式会社の基本情報と掲載情報</div>
+    <div class="agency_info_wrapper">
+        <input class="" type="text">
+    </div>
+</main>
+
+<?php include dirname(__FILE__) . '/footerPostEditAgency.php' ?>
