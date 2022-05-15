aa9b68f6429ce3df16532f07a6301141457ff5d3
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-15 14:12:24 +0900

    選択、編集をtop画面にも、またその他の処理も

--- src/admin/agencyInfo.php
+++ src/admin/agencyInfo.php
@@ -20,7 +20,7 @@ include dirname(__FILE__) . '/headerAgencyInfo.php';
         <div class="the_agency_info">
             <a href="./agencyInfo.php">○○○○llllllllllllll株式会社</a>
             <div>情報獲得数　2件</div>
-            <div>2022年　4月</div>
+            <div>期限　4/30</div>
             <div>金額xxxx</div>
             <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden"></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>">支払い済み</label>
         </div>
@@ -29,11 +29,11 @@ include dirname(__FILE__) . '/headerAgencyInfo.php';
             <!-- 仮で、支払い済みと未払いで分けています。どちらか、消してもらって大丈夫です。 -->
             <ul class="agency_list_inner">
                 <ol>
+                    <div>情報登録日時:　2022年3月5日</div>
                     <a href="">加茂竜之介</a>
-                    <div>情報獲得数　2件</div>
-                    <div>期限　4/30</div>
-                    <div>金額xxxx</div>
-                    <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden"></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>">支払い済み</label>
+                    <div>男性</div>
+                    <div>21歳</div>
+                    <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden" value="<?php echo $i ?>"></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>"></label>
                 </ol>
 
             </ul>
