4ed835c2b3d7218cb39bf6d1626d12fb661a32d8
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-15 19:41:50 +0900

    修正

---
+++ src/admin/agencyDetailed.php
@@ -0,0 +1,24 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+
+include dirname(__FILE__) . '/headerAgencyDetailed.php';
+?>
+
+<main>
+    <div class="content_wrapper">
+        <ul>
+            <li>氏名：加茂竜之介</li>
+            <li>Email：kamo@gamil.com</li>
+            <li>電話番号：090-3751-5188</li>
+            <li>大学名：早稲田大学</li>
+            <li>学科：教育学科</li>
+            <li>卒業年：25年卒</li>
+            <li>東京都xx区xx-xx-xx</li>
+         </ul>
+         <div class="num_sent_infromation">企業に応募した件数：４件</div>
+
+    </div>
+</main>
+
+
+<?php include dirname(__FILE__) . '/footerDetailed.php' ?>
