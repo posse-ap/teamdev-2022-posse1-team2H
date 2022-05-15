b05d61e6300b383d742310a4385aa0fcd0838f18
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-15 13:08:27 +0900

    さらに修正

--- src/index.php
+++ src/index.php
@@ -63,11 +63,7 @@ include dirname(__FILE__) . "/header.php";
                             <div class="agency_infromation">
                                 <div id="name_and_favorite" class="name_and_favorite">
                                     <div class="agency_name_wrapper"><span id="agency_name<?php echo $i ?>" class="agency_name">国際石油開発帝石ホールディングス</span></div>
-<<<<<<< HEAD
                                     <div id="star<?php echo $i ?>" class="star<?php echo $i?>" onclick="changingColor(this)"><i class="fa-solid fa-star"></i></div>
-=======
-                                    <div id="star<?php echo $i ?>" class="star" onclick="changingColor(this)"><i class="fa-solid fa-star"></i></div>
->>>>>>> af319d8 (とりあえずpush)
                                 </div>
                                 <div class="agency_slogan_wrapper"><span class="slogan">面接対策,ES添削で内定サポート！ 就活エージェントおすすめ15選</span></div>
                             </div>
