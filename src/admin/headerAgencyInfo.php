c4c2807565e389148bb670811349a0283782dc66
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-15 09:50:55 +0900

    一旦保存

--- src/admin/headerAgencyInfo.php
+++ src/admin/headerAgencyInfo.php
@@ -17,7 +17,7 @@
     <header>
         <div class="header_inner">
             <div class="page_name">管理画面</div>
-            <div id="delete" class="delete">
+            <div id="chaning_status" class="chaning_status_and_delete">
                 <button onclick="select()">選択</button>
                 <button onclick="edit()">編集</button>
                 <button onclick="deleting()">消去</button>
