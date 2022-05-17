6d4d0db987cf86efbed6b4e2bd9deaba7a5430eb
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-17 22:02:20 +0900

    refactor changing_color func

--- src/static/js/userTop.js
+++ src/static/js/userTop.js
@@ -1,67 +1,56 @@
 "use strict";
 
 const appearing = (appearing) => {
-    document.getElementById(appearing).classList.toggle('appearing');
-}
+  document.getElementById(appearing).classList.toggle("appearing");
+};
 
 const appearSidebar = () => {
-    appearing("serach_content");
+  appearing("serach_content");
 };
 
 const appearIndustryTypes = () => {
-    appearing("business_type_items");
+  appearing("business_type_items");
 };
 
 const appearTypes = () => {
-    appearing("business_features");
+  appearing("business_features");
 };
 
 let idArray = [];
 const A = async () => {
-    await axios('http://localhost/modules/api/user/firstView.php').then((res) => {
-
-        res.data.forEach(elem => {
-            let eachId = elem['id'];
-            // console.log(elem['id']);
-            idArray.push(eachId);
-        });
-        // console.log(idArray);
+  await axios("http://localhost/modules/api/user/firstView.php").then((res) => {
+    res.data.forEach((elem) => {
+      let eachId = elem["id"];
+      // console.log(elem['id']);
+      idArray.push(eachId);
     });
+    // console.log(idArray);
+  });
 };
-let newIdArray = [];
-const changingColor = (newColor) => {
-    const specifiedChangingColor = (new_color,i) => {
-        let new_color_id = document.getElementById(new_color);
-        if (new_color_id.className == 'star' + [i] + '') {
-            new_color_id.classList.add('changing_color');
-            newIdArray.push(idArray[i]);
-            Cookies.set('ids', `${newIdArray}`, { expires: 1 });
-            console.log(Cookies.get());
-        } else {
-            new_color_id.classList.remove('changing_color');
-            Cookies.remove('ids');
-            let slicedArray = newIdArray.slice(0, i);
-            console.log(slicedArray);
-            Cookies.set('ids', `${slicedArray}`, { expires: 1 });
-            console.log(Cookies.get());
-        }
 
-    };
-    for (let i = 0; i < 12; i++) {
-        if (newColor.id == 'star' + [i] + '') {
-            specifiedChangingColor(newColor.id,i);
-        };
-
-    };
+const handleClickStar = (agencyId) => {
+    toggleColor(agencyId)
 
 }
 
-const savingData = () => {
-    console.log(Cookies.get());
-    // console.log(Cookies.get('NAMEを入れて下さい'));
-    // 例↓
-    console.log(Cookies.get('ids'));
+const toggleColor = (agencyId) => {
+  let target = document.getElementById(`star_${agencyId}`);
+    target.classList.toggle("toggle_star");
+};
+
+const saveFav = (agencyId) => {
+  const agencyIds = readFav()
+  if (!agencyIds.includes(agencyId)) {
+      // add
+      agencyIds.push(agencyId)
+  } else {
+      agencyIds.splice(agencyIds.indexOf(agencyId), 1)
+  }
+};
+
+const readFav = () => {
+    let agencyIds = sessionStorage.getItem('ids')
+    agencyIds = agencyIds.split(',')
+    agencyIds = agencyIds.map(x => Number(x))
+    return agencyIds
 }
-
-
-window.onload = A();
