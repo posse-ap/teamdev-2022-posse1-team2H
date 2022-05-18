74f1e87814e9219700206fc3781df62b5e31a526
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-18 18:19:30 +0900

    prをもとに修正

--- src/admin/postEditAgencyinfo.php
+++ src/admin/postEditAgencyinfo.php
@@ -8,15 +8,20 @@ include dirname(__FILE__) . '/header.php';
     <header>
         <div class="header_inner">
             <div class="page_name">管理画面</div>
-            <a>基本情報・掲載情報</a>
+            <a href="./editableAndUneditableInfo.php">基本情報・掲載情報</a>
             <div style="font-weight:bold ;">編集・掲載</div>
         </div>
     </header>
     <main>
         <div id="displayed_content" class="displayed_content">○○○○株式会社の基本情報と掲載情報</div>
         <div class="agency_info_wrapper">
-            <input class="" type="text">
+            <ul class="agency_info_inner">
+                <li id="title">タイトル：<input type="text" name="" id=""></li>
+                <li id="sentenses">文章：<input type="text" name="" id=""></li>
+                <li id="eyecatch">画像：<input type="text" name="" id=""></li>
+            </ul>
         </div>
+        <button class="post_btn" onclick="post()">掲載</button>
     </main>
 
     <?php include dirname(__FILE__) . '/footer.php' ?>
