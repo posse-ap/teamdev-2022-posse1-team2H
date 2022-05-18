3a82fe48306223938ada284a0e6bcd2c9e79d133
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-18 15:14:19 +0900

    name属性を追加。feature/30ある程度完成

--- src/admin/agencyInfo.php
+++ src/admin/agencyInfo.php
@@ -41,7 +41,7 @@ include dirname(__FILE__) . '/header.php';
                         <a href="">加茂竜之介</a>
                         <div>男性</div>
                         <div>21歳</div>
-                        <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden"></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>"></label>
+                        <input id="checkbox<?php echo $i ?>" class="checkbox" type="hidden" name=""></input><label id="label<?php echo $i ?>" for="checkbox<?php echo $i ?>"></label>
 
                     </ol>
 
