184c5b6b23644a2b9fab48f11e4901d2aca353b1
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-18 17:00:03 +0900

    一旦保存

--- src/admin/postEditAgencyList.php
+++ src/admin/postEditAgencyList.php
@@ -1,31 +1,37 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
-include dirname(__FILE__) . '/headerPostEditAgencyList.php';
+include dirname(__FILE__) . '/header.php';
 ?>
 
-<main>
-    <div id="displayed_content" class="displayed_content">企業一覧</div>
-    <div class="agency_list_wrapper">
-        <ul class="agency_list_inner">
-            <ol>
-                <a href="./postEditAgencyinfo.php">○○○○llllllllllllll株式会社</a>
-                <div>編集・掲載依頼 4/1</div>
-            </ol>
-            
-            <ol>
-                <a href="./postEditAgencyinfo.php">○○○○llllllllllllll株式会社</a>
-                <div>基本情報変更 4/1</div>
-            </ol>
-            
-            <ol>
-                <a href="./postEditAgencyinfo.php">○○○○llllllllllllll株式会社</a>
-                <div>基本情報変更 4/13 　編集・掲載依頼4/5</div>
-            </ol>
+<body>
+    <header>
+        <div class="header_inner">
+            <div class="page_name">管理画面</div>
+        </div>
+    </header>
+    <main>
+        <div id="displayed_content" class="displayed_content">企業一覧</div>
+        <div class="agency_list_wrapper">
+            <ul class="agency_list_inner">
+                <ol>
+                    <a href="./postEditAgencyinfo.php">○○○○llllllllllllll株式会社</a>
+                    <div>編集・掲載依頼 4/1</div>
+                </ol>
 
-        </ul>
+                <ol>
+                    <a href="./postEditAgencyinfo.php">○○○○llllllllllllll株式会社</a>
+                    <div>基本情報変更 4/1</div>
+                </ol>
 
-    </div>
-</main>
+                <ol>
+                    <a href="./postEditAgencyinfo.php">○○○○llllllllllllll株式会社</a>
+                    <div>基本情報変更 4/13 　編集・掲載依頼4/5</div>
+                </ol>
 
-<?php include dirname(__FILE__) . '/footerPostEditAgencyList.php' ?>
+            </ul>
+
+        </div>
+    </main>
+
+    <?php include dirname(__FILE__) . '/footer.php' ?>
