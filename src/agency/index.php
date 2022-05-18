e443a383a54633436e345f1b8c647fc042b774b0
mirei1115 <wanima9990@gmail.com>
2022-05-18 17:22:06 +0900

    一旦push

--- src/agency/index.php
+++ src/agency/index.php
@@ -7,8 +7,8 @@ $auth->validate();
 
 include dirname(__FILE__) . '/header.php' ?>
 
-<div class="export-button"><a href="">エクスポート</a></div>
-<div class="private-big-box">
+<!-- <div class="export-button"><a href="">エクスポート</a></div> -->
+<div>
     <p class="private-small-box"><a href="detail.php">2022/01/22 22:39 田中太郎 男 21歳 taro.tanaka@gmail.com</a></p>
     <p class="private-small-box"><a href="">2022/01/22 21:35 山田桜 女 22歳 sakura.yamada@gmail.com</a></p>
     <p class="private-small-box"><a href="">2022/01/20 23:59 鈴木誠也 男 22歳 seiya.suzuki@gmail.com</a></p>
