7a1f17af62c3963720d473e39ab872cad67da7dd
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-18 16:30:47 +0900

    消去ボタンを押した時、typeがチェックボックスからhiddenに変更されるようにした

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -10,11 +10,14 @@ const deleting = () => {
     for (let i = 1; i < 6; i++) {
         let checkbox = document.getElementById(`checkbox${i}`);
         checkbox.type = "checkbox";
-    }
-    // ここから先はお任せします。
-    if (checkbox.checked) {
 
-    } else {
+        // ここから先はお任せします。
+        if (checkbox.checked) {
 
+        } else {
+
+        }
+
+        checkbox.type = "hidden";
     }
 }
