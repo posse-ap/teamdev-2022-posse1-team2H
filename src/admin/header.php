95dc36ca93e8b0eef4009417c5a571864a9a210a
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-20 10:34:08 +0900

    パスの修正

--- src/admin/header.php
+++ src/admin/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../static/css/sanitize.css">
-    <link rel="stylesheet" href="../static/./css/./admin.css">
+    <link rel="stylesheet" href="../static/css/admin.css">
     <script src="https://kit.fontawesome.com/727d59e43e.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <title>Craft for Boozer</title>
