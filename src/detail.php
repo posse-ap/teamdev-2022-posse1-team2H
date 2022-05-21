2d9b9da494eacd01ce4c96db8df8242182507c5e
rikuto-nonu <20ca005p@rikkyo.ac.jp>
2022-05-22 03:31:34 +0900

    レスポンシブ以外完成しました

--- src/detail.php
+++ src/detail.php
@@ -22,14 +22,14 @@ include dirname(__FILE__) . '/header.php';
 <div class="content_detail">
             <div class="content_detail_inner">
                 <!-- キャッチコピーと写真 -->
-                <div class="content_detail_catchCopy_imgBig">
-                    <div class="content_detail_catchCopy">
-                        <h2 class="content_detail_catchCopy_title">とにかく文系に強い！！</h2>
-                        <p class="content_detail_catchCopy_subtitle">#医療＃外資系</p>
+                <div class="content_detail_catchCopy_imgbig">
+                    <div class="content_detail_catchcopy">
+                        <h2 class="content_detail_catchcopy_title">とにかく文系に強い！！</h2>
+                        <p class="content_detail_catchcopy_subtitle">#医療＃外資系</p>
                     </div>
                     <div class="content_detail_agency_imgBig">
-                        <a href="https://posse-ap.com/" class="content_detail_agency_imgBig_">
-                            <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_agency_imgBig">
+                        <a href="https://posse-ap.com/" class="content_detail_agency_imgbig_">
+                            <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_agency_imgbig">
                         </a>
                     </div>
 
@@ -47,8 +47,8 @@ include dirname(__FILE__) . '/header.php';
                                <li class="content_detail_subcontent_left_agentStudent_table_detail">詳細ページ</li>
                             </ul>
                             <div class="content_detail_subcontent_left_agentStudent_agent">
-                                <h1 class="content_detail_subcontent_left_agentRequest_agent_title">どんなエージェント企業か</h1>
-                                <p class="content_detail_subcontent_left_agentRequest_agent_text">
+                                <h1 class="content_detail_subcontent_left_agentrequest_agent_title">どんなエージェント企業か</h1>
+                                <p class="content_detail_subcontent_left_agentrequest_agent_text">
                                     アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。 一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。 「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは思いました。 そこでアリスは、頭のなかで、ひなぎくのくさりをつくったら楽しいだろうけれど、 起きあがってひなぎくをつむのもめんどくさいし、どうしようかと考えていました （といっても、昼間で暑いし、とってもねむくて頭もまわらなかったので、
                                     これもたいへんだったのですが）。 そこへいきなり、ピンクの目をした白うさぎが近くを走ってきたのです。 それだけなら、そんなにめずらしいことでもありませんでした。 さらにアリスとしては、そのうさぎが 「どうしよう！ どうしよう！ ちこくしちゃうぞ！」 とつぶやくのを聞いたときも、 それがそんなにへんてこだとは思いませんでした （あとから考えてみたら、これも不思議に思うべきだったのですけれど、 でもこのときには、それがごく自然なことに思えたのです）。
                                 </p>
@@ -62,25 +62,25 @@ include dirname(__FILE__) . '/header.php';
                             </div>
                         </div>
                         <div class="borderline"><hr></div>
-                        <div class="content_detail_recommendedAgent">
-                            <h2 class="content_detail_recommendedAgent_title">あなたにオススメのエージェンシー
+                        <div class="content_detail_recommendedagent">
+                            <h2 class="content_detail_recommendedagent_title">あなたにオススメのエージェンシー
                             </h2>
-                            <div class="content_detail_recommendedAgent_bigBox">
-                                <article class="content_detail_recommendedAgent_box1">
-                                    <div class="content_detail_recommendedAgent_box1_img"></div>
+                            <div class="content_detail_recommendedagent_bigbox">
+                                <article class="content_detail_recommendedagent_box1">
+                                    <div class="content_detail_recommendedagent_box1_img"></div>
                                     <a href="https://posse-ap.com/">
-                                        <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedAgent_box1_img"></a>
+                                        <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedagent_box1_img"></a>
 
                                 </article>
-                                <article class="content_detail_recommendedAgent_box2">
-                                    <div class="content_detail_recommendedAgent_box2_img"></div>
+                                <article class="content_detail_recommendedagent_box2">
+                                    <div class="content_detail_recommendedagent_box2_img"></div>
                                     <a href="https://posse-ap.com/">
-                                        <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedAgent_box2_img"></a>
+                                        <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedagent_box2_img"></a>
                                 </article>
-                                <article class="content_detail_recommendedAgent_box3">
-                                    <div class="content_detail_recommendedAgent_box3_img"></div>
+                                <article class="content_detail_recommendedagent_box3">
+                                    <div class="content_detail_recommendedagent_box3_img"></div>
                                     <a href="https://posse-ap.com/">
-                                        <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedAgent_box3_img"></a>
+                                        <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedagent_box3_img"></a>
                                 </article>
 
                             </div>
@@ -93,7 +93,7 @@ include dirname(__FILE__) . '/header.php';
                             <div class="content_detail_subcontent_right_serch_menu">
                                 <div class="content_detail_subcontent_right_serch_menu_text">メニュー</div>
                             </div>
-                            <div class="content_detail_subcontent_right_serch_inquiryBox">
+                            <div class="content_detail_subcontent_right_serch_inquirybox">
                                 <h3 class="content_detail_subcontent_right_serch_inquiryText">お問合せ</h3>
                             </div>
                             <div class="content_detail_subcontent_right_serch_seelaterField">
@@ -103,8 +103,8 @@ include dirname(__FILE__) . '/header.php';
                                         <h3 class="content_detail_subcontent_right_serch_seelaterField_add_box_text">気になるリストへの追加</h3>
                                     </div>
                                 </div>
-                                <div class="content_detail_subcontent_right_serch_seelaterField_seelaterBox">
-                                    <h3 class="content_detail_subcontent_right_serch_seelaterField_seelaterBox_text">気になる
+                                <div class="content_detail_subcontent_right_serch_seelaterField_seelaterbox">
+                                    <h3 class="content_detail_subcontent_right_serch_seelaterField_seelaterbox_text">気になる
                                     </h3>
 
                                 </div>
@@ -136,17 +136,17 @@ include dirname(__FILE__) . '/header.php';
                 </div>
                 <!-- 一番下に固定しておく後で見るに追加とお問合せ -->
 
-                <div class="content_detail_underRequest">
+                <div class="content_detail_underrequest">
                     <div class="content_detail_action_bar">
 
-                        <div class="content_detail_underRequest_seelaterBox">
+                        <div class="content_detail_underrequest_seelaterbox">
 
-                            <h4 class="content_detail_underRequest_seelaterText">後で見るリストに追加
+                            <h4 class="content_detail_underrequest_seelaterText">後で見るリストに追加
                             </h4>
                         </div>
-                        <div class="content_detail_underRequest_inquiryBox">
+                        <div class="content_detail_underrequest_inquirybox">
 
-                            <h4 class="content_detail_underRequest_inquiryText">お問合せ
+                            <h4 class="content_detail_underrequest_inquiryText">お問合せ
                             </h4>
                         </div>
                     </div>
