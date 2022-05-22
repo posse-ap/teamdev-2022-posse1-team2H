ac475876219d2707027ba67889678083020634b1
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-22 11:08:35 +0900

    cssなどもろもろ修正

--- src/seeLater.php
+++ src/seeLater.php
@@ -15,91 +15,57 @@ include dirname(__FILE__) . "/header.php";
 ?>
 <main class="user_likelist">
   <div class="user_likelist_topbar">
-<a href="./index.php" class="user_likelist_topbar_top">TOP画面へ</a>
-    
-    <button class="user_likelist_topbar_inquiry">まとめてお問合せ
+    <a href="./index.php" class="user_likelist_topbar_top">TOP画面へ</a>
+
+    <button class="user_likelist_topbar_inquiry">
+      <a href="./contact.php">まとめて問い合わせる</a>
     </button>
   </div>
   <div class="user_likelist_frame">
     <div class="user_likelist_number">
-  <h3 class="user_likelist_number"><span class="user_likelist_number_Z">4</span>件</h3>
+      <h3 class="user_likelist_number"><span class="user_likelist_number_Z">4</span>件</h3>
     </div>
     <div class="user_likelist_inner">
       <div class="user_likelist_inquiry">
       </div>
-        <div class="user_likelist_inner1box">
-          <div class="user_likelist_inner1">
-            <div class="user_likelist_inner1_header">
-                <h2 class="user_likelist_inner1_header_title">株式会社武田鉄</h2>
-                <div class="user_likelist_inner1_trash">
-                    <h3 class="user_likelist_inner1_trash_text">削除する</h3>
-                </div>
+      <div class="user_likelist_inner1box">
+        <div class="user_likelist_inner1">
+          <div class="user_likelist_inner1_header">
+            <h2 class="user_likelist_inner1_header_title">株式会社武田鉄</h2>
+            <div class="user_likelist_inner1_trash">
+              <h3 class="user_likelist_inner1_trash_text">削除する</h3>
             </div>
-            <div class="user_likelist_inner1_body">
-                
-                    <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="user_likelist_inner1_body_img">
-           
-                <div class="user_likelist_inner1_body_right">
-                <div class="user_likelist_inner1_body_text">
-                      <p class="user_likelist_inner1_body_text_catchcopy">とにかく文系に強い！！！！</p>
-                      <p class="user_likelist_inner1_body_text_connection">#機械系#パワー型</p>
-                    </div>
+          </div>
+          <div class="user_likelist_inner1_body">
 
-                    <div class="user_likelist_inner1_body_under">
-                        <button class="user_likelist_inner1_body_under_contact">
-                          お問合せる
-                        </button>
-                        <button class="user_likelist_inner1_body_under_detail">
-                          詳細ページへ
-                        </button>
-                    </div>
+            <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="user_likelist_inner1_body_img">
+
+            <div class="user_likelist_inner1_body_right">
+              <div class="user_likelist_inner1_body_text">
+                <p class="user_likelist_inner1_body_text_catchcopy">とにかく文系に強い！！！！</p>
+                <div class="user_likelist_inner1_body_text_connection">
+                  <a href="">#機械系</a>
+                  <a href="">#パワー型</a>
                 </div>
-            </div>
+              </div>
 
+              <div class="user_likelist_inner1_body_under">
+                <button class="user_likelist_inner1_body_under_contact">
+                  <a href="./contact.php">問い合わせる</a>
+                </button>
+                <button class="user_likelist_inner1_body_under_detail">
+                  <a href="./detail.php">詳細ページ</a>
+                </button>
+              </div>
+            </div>
           </div>
 
         </div>
-    </div>
 
-  </div>
-  <div class="user_likelist_frame">
-    <div class="user_likelist_inner">
-      <div class="user_likelist_inquiry">
       </div>
-        <div class="user_likelist_inner1box">
-          <div class="user_likelist_inner1">
-            <div class="user_likelist_inner1_header">
-                <h2 class="user_likelist_inner1_header_title">株式会社武田鉄</h2>
-                <div class="user_likelist_inner1_trash">
-                    <h3 class="user_likelist_inner1_trash_text">削除する</h3>
-                </div>
-            </div>
-            <div class="user_likelist_inner1_body">
-                
-                    <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="user_likelist_inner1_body_img">
-           
-                <div class="user_likelist_inner1_body_right">
-                    <div class="user_likelist_inner1_body_text">
-                      <p class="user_likelist_inner1_body_text_catchcopy">とにかく文系に強い！！！！</p>
-                      <p class="user_likelist_inner1_body_text_connection">#機械系#パワー型</p>
-                    </div>
-
-                    <div class="user_likelist_inner1_body_under">
-                        <button class="user_likelist_inner1_body_under_contact">
-                          お問合せる
-                        </button>
-                        <button class="user_likelist_inner1_body_under_detail">
-                          詳細ページへ
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-          </div>
-
-        </div>
     </div>
 
   </div>
-    </main>
+
+</main>
 <?php include dirname(__FILE__) . '/footer.php' ?>
