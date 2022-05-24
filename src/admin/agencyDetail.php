65ff134924502fe31fc365e3b6967c7688e443fa
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-24 22:56:21 +0900

    agenciesあたりを結合

--- src/admin/agencyDetail.php
+++ src/admin/agencyDetail.php
@@ -12,7 +12,7 @@ $auth->validate();
 $id = $_GET['id'];
 
 if (!isset($id)) {
-    header('Location: index.php');
+    header('Location: agencies.php');
 }
 
 $agency = $cruds->getAgencyDetail($id);
@@ -25,27 +25,25 @@ include dirname(__FILE__) . '/header.php';
 <body>
     <header>
         <div class="header_inner">
-            <div class="page_name">○○○○株式会社の詳細情報</div>
-            <a href="./index.php">初期画面へ</a>
-            <a href="./logout.php">logout</a>
+            <div class="page_name"><?= $agency->name ?>の詳細情報
+        </div>
+        <a href="./index.php">TOP画面へ</a>
+        <a href="./logout.php">logout</a>
         </div>
     </header>
     <main>
         <div class="content_wrapper">
             <ul class="content_inner">
-                <li id="name">企業名：</li>
-                <li id="email">Email：</li>
-                <li id="email_for_notification">通知用Email：</li>
-                <li id="tel">電話番号：</li>
-                <li id="url">url：</li>
-                <li id="representative">代表者：</li>
-                <li id="contactor">契約担当者：</li>
-                <li id="address">住所：</li>
-                <li id="address_num">番地：</li>
+                <li id="name">企業名：<?= $agency->name ?></li>
+                <li id="email">Email：<?= $agency->email ?></li>
+                <li id="email_for_notification">通知用Email：<?= $agency->email_for_notice ?></li>
+                <li id="tel">電話番号：<?= $agency->tel ?></li>
+                <li id="url">url：<?= $agency->url ?></li>
+                <li id="representative">代表者：<?= $agency->representative ?></li>
+                <li id="contactor">契約担当者：<?= $agency->contactor ?></li>
+                <li id="address">住所：<?= $agency->address ?></li>
+                <li id="address_num">郵便番号：<?= $agency->address_num ?></li>
             </ul>
         </div>
     </main>
-
-    <!-- <?php var_dump($agency)  // for debug 
-    ?> -->
     <?php include dirname(__FILE__) . '/footer.php' ?>
--- src/admin/agencyInfo.php
+++ src/admin/agencyInfo.php
@@ -1,6 +1,24 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
+use modules\auth\Admin as Auth;
+use cruds\Admin as Cruds;
+
+$auth = new Auth($db);
+$cruds = new Cruds($db);
+
+$auth->validate();
+
+$id = $_GET['id'];
+
+if (!isset($id)) {
+    header('Location: agencies.php');
+}
+
+$agency = $cruds->getAgencyDetail($id);
+
+$agency = json_decode($agency);
+
 include dirname(__FILE__) . '/header.php';
 ?>
 
@@ -9,40 +27,33 @@ include dirname(__FILE__) . '/header.php';
         <div class="inside_header">
             <div class="page_name">管理画面</div>
             <div class="title_name">基本情報・掲載情報</div>
-            <a href="./edit.php">編集・掲載へ</a>
+            <a href="./edit.php?id=<?= $agency->agency_id ?>">編集・掲載へ</a>
             <a href="./agencies.php">企業一覧へ</a>
             <a href="./logout.php">logout</a>
         </div>
     </header>
     <main>
-        <div id="displayed_content" class="displayed_content">○○○○株式会社の基本情報と掲載情報</div>
+        <div id="displayed_content" class="displayed_content"><?= $agency->name ?></div>
         <div class="agency_info_wrapper">
             <div>
                 <div class="editable_info">
                     <div class="agency_info_type">エージェンシー編集可能情報</div>
-                    <ul>
-                        <li id="name">企業名：</li>
-                        <li id="email">Email：</li>
-                        <li id="email_for_notification">通知用Email：</li>
-                        <li id="tel">電話番号：</li>
-                        <li id="url">url：</li>
-                        <li id="representative">代表者：</li>
-                        <li id="contactor">契約担当者：</li>
-                        <li id="address">住所：</li>
-                        <li id="address_num">番地：</li>
+                    <ul class="content_inner">
+                        <li id="name">企業名：<?= $agency->name ?></li>
+                        <li id="email">Email：<?= $agency->email ?></li>
+                        <li id="email_for_notification">通知用Email：<?= $agency->email_for_notice ?></li>
+                        <li id="tel">電話番号：<?= $agency->tel ?></li>
+                        <li id="url">url：<?= $agency->url ?></li>
+                        <li id="representative">代表者：<?= $agency->representative ?></li>
+                        <li id="contactor">契約担当者：<?= $agency->contactor ?></li>
+                        <li id="address">住所：<?= $agency->address ?></li>
+                        <li id="address_num">郵便番号：<?= $agency->address_num ?></li>
                     </ul>
                 </div>
                 <div class="uneditable_info">
-                    <div class="agency_info_type">掲載されている情報</div>
+                    <div class="agency_info_type">掲載記事</div>
                     <div class="uneditable_info_content">
-                        むかしむかし、あるところに、おじいさんとおばあさんが住んでいました。
-                        　おじいさんは山へしばかりに、おばあさんは川へせんたくに行きました。
-                        　おばあさんが川で洗濯をしていると、ドンブラコ、ドンブラコと、大きな桃が流れてきました。
-                        「おや、これは良いおみやげになるわ」
-                        　おばあさんは大きな桃をひろいあげて、家に持ち帰りました。
-                        　そして、おじいさんとおばあさんが桃を食べようと桃を切ってみると、なんと、中から元気の良い男の赤ちゃんが飛び出してきました。
-                        「これはきっと、神さまがくださったにちがいない」
-                        　子どものいなかったおじいさんとおばあさんは、大喜びです。
+                    <?= $agency->sentenses ?>
                     </div>
                 </div>
             </div>
