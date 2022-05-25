553ef53c0eedb22785e145a1ae3307771a253b6e
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-25 21:51:55 +0900

    rename api

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -96,7 +96,7 @@ const time = () => {
     today.push(month);
   }
   let dateToday = today.join(",").replaceAll(",", "-");
-  console.log(dateToday);
+//   console.log(dateToday);
   document.getElementById("date_today").value = dateToday;
 };
 
@@ -106,7 +106,7 @@ const getContractId = () => {
 };
 
 const getAgenciesForFirstView = async () => {
-  const { data } = await request.get({ url: `${prefix}/firstView.php` });
+  const { data } = await request.get({ url: `${prefix}/contracts.php` });
   drawHTMLs.contracts(data);
 };
 
