64b9327ccee6089e6dea626d2b9b671474c6b26a
加茂竜之介 <Kamoryu2001@gmail.com>
2022-05-28 20:46:28 +0900

    ぬのっちの引継ぎ完了

--- src/index.php
+++ src/index.php
@@ -14,17 +14,25 @@ $industries = $cruds->getIndustries();
 include dirname(__FILE__) . "/header.php";
 ?>
 <main id="user_top" class="content">
+
     <!-- エージェント一覧 -->
     <div class="agency_list">
         <div class="new_agency_wrapper">
-            <h1 class="title">新着</h1>
+            <div class="title_box">
+                <h1 class="title">NEW ARRIVAL</h1>
+                <h3 class="subtitle">新着</h3>
+            </div>
             <div class="new_agency_inner" id="new_agencies_target">
-                    <!-- articles -->
+                <!-- articles -->
             </div>
         </div>
 
         <div class="popular_agency_wrapper">
-            <h1 class="title">人気</h1>
+            <!-- <h1 class="title">人気</h1> -->
+            <div class="title_box">
+                <h1 class="title">RANKING</h1>
+                <h3 class="subtitle">ランキング</h3>
+            </div>
             <div class="popular_agency_inner">
                 <?php for ($i = 6; $i < 12; $i++) : ?>
                     <article class="popular_agency_card">
@@ -62,39 +70,39 @@ include dirname(__FILE__) . "/header.php";
         <aside id="sidebar_inner" class="sidebar_inner">
             <div class="sidebar_favorite_area">
                 <div class="sidebar_favorite">
-                    <a href="seeLater.php">後で見る</a>
+                    <a href="seeLater.php" class="sidebar_favorite_text">気になるリストへ</a>
                 </div>
                 <div class="favorite_content"></div>
-            </div><br>
-            <div class="sidebar_search_area">
-                <form name="searchform" method="" action="" onsubmit="return false">
-                    <div class="sidebar_search">
-                        <h2 onclick="appearSidebar()">検索</h2>
-                        <div id="serach_content" class="serach_content">
-                            <div id="business_type_wrapper" class="business_type_wrapper">
-                                <h3 onclick="appearIndustryTypes()"><i class="fa-solid fa-building"> 業種</i></h3>
-                                <div id="business_type_items" class="business_type_inner">
-                                    <?php foreach ($industries as $industry) : ?>
-                                        <label>
-                                            <input class="business_type_tag" type="checkbox" name="industries" value="<?= $industry['id'] ?>" onclick="handleSearch()"><?= $industry['industry'] ?>
-                                        </label>
-                                    <?php endforeach ?>
+                <!-- </div><br> -->
+                <div class="sidebar_search_area">
+                    <form name="searchform" method="" action="" onsubmit="return false">
+                        <div class="sidebar_search">
+                            <h1 onclick="appearSidebar()" class="sidebar_search_text">検索</h1>
+                            <div id="serach_content" class="serach_content">
+                                <div id="business_type_wrapper" class="business_type_wrapper">
+                                    <h3 onclick="appearIndustryTypes()" class="business_type_wrapper_text"><i class="fa-solid fa-building"> 業種</i></h3>
+                                    <div id="business_type_items" class="business_type_inner">
+                                        <?php foreach ($industries as $industry) : ?>
+                                            <label class="a">
+                                                <input class="business_type_tag" type="checkbox" name="industries" value="<?= $industry['id'] ?>" onclick="handleSearch()"><?= $industry['industry'] ?>
+                                            </label>
+                                        <?php endforeach ?>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="business_features_wrapper">
-                                <h3 onclick="appearTypes()"><i class="fa-solid fa-glasses"></i> 特徴</h3>
-                                <div id="business_features" class="business_type_inner">
-                                    <?php foreach ($types as $type) : ?>
-                                        <label>
-                                            <input class="business_type_tag" type="checkbox" name="types" value="<?= $type['id'] ?>" onclick="handleSearch()"><?= $type['agency_type'] ?>
-                                        </label>
-                                    <?php endforeach ?>
+                                <div class="business_features_wrapper">
+                                    <h3 onclick="appearTypes()" class="business_features_wrapper_text"><i class="fa-solid fa-glasses"></i> 特徴</h3>
+                                    <div id="business_features" class="business_type_inner">
+                                        <?php foreach ($types as $type) : ?>
+                                            <label>
+                                                <input class="business_type_tag" type="checkbox" name="types" value="<?= $type['id'] ?>" onclick="handleSearch()"><?= $type['agency_type'] ?>
+                                            </label>
+                                        <?php endforeach ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
         </aside>
     </div>
 
@@ -103,7 +111,7 @@ include dirname(__FILE__) . "/header.php";
 <div class="bar_for_responsive">
     <div class="bar_for_responsive_inner">
         <a href="" class="back_to_top"><i id="home_icon" class="fa-solid fa-square-caret-up"></i></a>
-        <a href="./seeLater.php" class="back_to_top"><i id="book_mark" class="fa-solid fa-book-bookmark"></i></a>
+        <a href="./seeLater.php" class="back_to_top"><i id="book_mark" class="fas fa-bookmark"></i></a>
         <div class="appearing_serach_area" onclick="dispalyingSerachArea()"><i id="serach_icon" class="fa-solid fa-magnifying-glass"></i></div>
     </div>
 </div>
@@ -136,6 +144,9 @@ include dirname(__FILE__) . "/header.php";
                             <?php endforeach ?>
                         </div>
                     </div>
+                </div>
+                <div class="modal_search">
+                    <a href="" class="modal_search_text">検索</a>
                 </div>
             </div>
         </form>
