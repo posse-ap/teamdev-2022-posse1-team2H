9803f80c0eadce66e81585e48d985e0f5c9d1b11
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-28 09:06:29 +0900

    文言の修正

--- src/admin/index.php
+++ src/admin/index.php
@@ -16,10 +16,10 @@ include dirname(__FILE__) . '/header.php';
 
     <header>
         <div class="header_inner">
-            <div class="page_name">管理画面</div>
+            <div class="page_name">Craft For Administrator</div>
             <nav>
                 <ul class="for_transition">
-                    <li><a href="./agencies.php">掲載・編集関連画面へ</a></li>
+                    <li><a href="./agencies.php">エージェンシー情報編集</a></li>
                 </ul>
             </nav>
         </div>
