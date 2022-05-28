a32c7e989ab95e6a75bf69c9647bf264e4e19af3
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-28 17:48:35 +0900

    add manager

--- src/agency/admininfo.php
+++ src/agency/admininfo.php
@@ -7,6 +7,34 @@ $crud = new Crud($db);
 $auth = new Agency($db);
 $auth->validate();
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    if (empty($name)) {
+        $error['name'] = 'blank';
+    }
+    if (empty($email)) {
+        $error['email'] = 'blank';
+    }
+    if (empty($password)) {
+        $error['password'] = 'blank';
+    }
+    if (empty($confirm_password)) {
+        $error['confirm_password'] = 'blank';
+    }
+    if ($password !== $confirm_password) {
+        $error['password'] = 'failed';
+    }
+    if (empty($error)) {
+        $_SESSION['manager_add']['name'] = $name;
+        $_SESSION['manager_add']['email'] = $email;
+        $_SESSION['manager_add']['password'] = $password;
+        header('Location:http://' . $_SERVER['HTTP_HOST'] . '/agency/addConfirm.php');
+        exit();
+    }
+}
 $agency_id = $_SESSION['agency']['id'];
 $manager_id = $_SESSION['agency_manager']['id'];
 
@@ -30,13 +58,13 @@ include dirname(__FILE__) . '/header.php' ?>
 <div id="overlay" class="overlay"></div>
 <div id="modal" class="modal">
     <div onclick="closingBtn()" id="close_btn" class="close_btn"></div>
-    <div id="modal_inner" class="modal_inner">
+    <form action="" method="POST" id="modal_inner" class="modal_inner">
         <h1 class="modal_title">個人担当者追加</h1>
         <dl class="add_box">
             <dt>お名前</dt>
             <dd class="name_box inquary_content_inner_name_enter">
                 <span class="inquary_content_inner_name_enter_box">
-                    <input type="text" value size="40" class="inquary_content_inner_name_enter_text">
+                    <input type="text" name="name"size="40" class="inquary_content_inner_name_enter_text" placeholder="※">
                 </span>
             </dd>
         </dl>
@@ -44,7 +72,7 @@ include dirname(__FILE__) . '/header.php' ?>
             <dt>Email</dt>
             <dd class="email_box inquary_content_inner_mail_enter">
                 <span class="inquary_content_inner_mail_enter_box">
-                    <input type="text" value size="40" class="inquary_content_inner_mail_enter_text" aria-required="true" aria-invalid="false" placeholder="※半角数字">
+                    <input type="text" name="email" size="40" class="inquary_content_inner_mail_enter_text" aria-required="true" aria-invalid="false" placeholder="※半角英数字">
                 </span>
             </dd>
         </dl>
@@ -52,7 +80,7 @@ include dirname(__FILE__) . '/header.php' ?>
             <dt>パスワード</dt>
             <dd class="password_box inquary_content_inner_mail_enter">
                 <span class="inquary_content_inner_mail_enter_box">
-                    <input type="password" value size="40" class="inquary_content_inner_mail_enter_text" aria-required="true" aria-invalid="false" placeholder="※半角数字">
+                    <input type="password" name="password" size="40" class="inquary_content_inner_mail_enter_text" aria-required="true" aria-invalid="false" placeholder="※">
                 </span>
             </dd>
         </dl>
@@ -60,12 +88,11 @@ include dirname(__FILE__) . '/header.php' ?>
             <dt>パスワード確認</dt>
             <dd class="password_confirm_box inquary_content_inner_mail_enter">
                 <span class="inquary_content_inner_mail_enter_box">
-                    <input type="password" value size="40" class="inquary_content_inner_mail_enter_text" aria-required="true" aria-invalid="false" placeholder="※半角数字">
+                    <input type="password" name="confirm_password" size="40" class="inquary_content_inner_mail_enter_text" aria-required="true" aria-invalid="false" placeholder="※">
                 </span>
             </dd>
         </dl>
-        <a class="confirm_button" href="addConfirm.php">確認</a>
-    </div>
+        <input type="submit" value="確認">
 </div>
 
 <?php include dirname(__FILE__) . '/footer.php' ?>
