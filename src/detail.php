92289ee83677a6523d3751f4e32c19c2a88d4504
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-29 09:15:25 +0900

    detailの表示を修正"

--- src/detail.php
+++ src/detail.php
@@ -26,7 +26,7 @@ include dirname(__FILE__) . '/header.php';
             <!-- キャッチコピーと写真 -->
             <div class="content_detail_catchcopy_imgbig">
                 <div class="content_detail_catchcopy">
-                    <h1 class="content_detail_catchcopy_title"><?= Utils::h($agency->title) ?></h1>
+                    <h1 class="content_detail_catchcopy_title"><?= Utils::h($agency->name) ?></h1>
                     <div class="content_detail_catchcopy_subtitle">
                         <ul class="content_detail_catchcopy_subtitle_items">
                         <?php foreach ($agency->industries as $industry) : ?>
@@ -57,20 +57,18 @@ include dirname(__FILE__) . '/header.php';
                             <li class="content_detail_subcontent_left_agentstudent_table_detail">詳細ページ</li>
                         </ul>
                         <div class="content_detail_subcontent_left_agentstudent_agent">
-                            <h2 class="content_detail_subcontent_left_agentrequest_agent_title">どんなエージェント企業か</h2>
+                            <h1 class="content_detail_subcontent_left_agentrequest_agent_title"><?= Utils::h($agency->title) ?></h1>
                             <p class="content_detail_subcontent_left_agentrequest_agent_text">
-                                アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。 一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。 「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは思いました。 そこでアリスは、頭のなかで、ひなぎくのくさりをつくったら楽しいだろうけれど、 起きあがってひなぎくをつむのもめんどくさいし、どうしようかと考えていました （といっても、昼間で暑いし、とってもねむくて頭もまわらなかったので、
-                                これもたいへんだったのですが）。 そこへいきなり、ピンクの目をした白うさぎが近くを走ってきたのです。 それだけなら、そんなにめずらしいことでもありませんでした。 さらにアリスとしては、そのうさぎが 「どうしよう！ どうしよう！ ちこくしちゃうぞ！」 とつぶやくのを聞いたときも、 それがそんなにへんてこだとは思いませんでした （あとから考えてみたら、これも不思議に思うべきだったのですけれど、 でもこのときには、それがごく自然なことに思えたのです）。
+                            <?= Utils::h($agency->sentenses) ?>
                             </p>
-                        </div>
-                        <div class="borderline">
-                            <hr>
-                        </div>
-                        <div class="content_detail_subcontent_left_agentstudent_student">
-                            <h2 class="content_detail_subcontent_left_agentstudent_student_title">どんな学生にオススメか</h2>
-                            <p class="content_detail_subcontent_left_agentstudent_student_text">
-                                アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。 一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。 「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは思いました。 そこでアリスは、頭のなかで、ひなぎくのくさりをつくったら楽しいだろうけれど、 起きあがってひなぎくをつむのもめんどくさいし、どうしようかと考えていました （といっても、昼間で暑いし、とってもねむくて頭もまわらなかったので、
-                                これもたいへんだったのですが）。 そこへいきなり、ピンクの目をした白うさぎが近くを走ってきたのです。 それだけなら、そんなにめずらしいことでもありませんでした。 さらにアリスとしては、そのうさぎが 「どうしよう！どうしよう！ ちこくしちゃうぞ！」 とつぶやくのを聞いたときも、 それがそんなにへんてこだとは思いませんでした （あとから考えてみたら、これも不思議に思うべきだったのですけれど、 でもこのときには、それがごく自然なことに思えたのです
+                            <p class="content_detail_subcontent_left_agentrequest_agent_text">
+                            <?= Utils::h($agency->sentenses) ?>
+                            </p>
+                            <p class="content_detail_subcontent_left_agentrequest_agent_text">
+                            <?= Utils::h($agency->sentenses) ?>
+                            </p>
+                            <p class="content_detail_subcontent_left_agentrequest_agent_text">
+                            <?= Utils::h($agency->sentenses) ?>
                             </p>
                         </div>
                         <div class="content_detail_subcontent_left_responsive_company">
@@ -81,39 +79,17 @@ include dirname(__FILE__) . '/header.php';
                                 <div class="content_detail_subcontent_left_responsive_company_text">
                                     <p class="content_detail_subcontent_left_responsive_company_companyname">
 
-                                        <a href="https://google.com" class="company_name">株式会社武田鉄</a>
+                                        <a href="<?= Utils::h($agency->url) ?>" class="company_name"><?= Utils::h($agency->name) ?></a>
                                     </p>
-                                    <p class="content_detail_subcontent_left_responsive_company_found">2020年に創業</p>
-                                    <p class="content_detail_subcontent_left_responsive_company_member">15000のメンバー</p>
-                                    <p class="content_detail_subcontent_left_responsive_company_address">東京都港区表参道Harbors</p>
+                                    <p><?= Utils::h($agency->email) ?></p>
+                                <p><?= Utils::h($agency->tel) ?></p>
+                                <p><?= Utils::h($agency->url) ?></p>
+                                    <p class="content_detail_subcontent_left_responsive_company_address"><?= Utils::h($agency->address) ?></p>
                                 </div>
                         </div>
                     </div>
                     <div class="borderline">
                         <hr>
-                    </div>
-                    <div class="content_detail_recommendedagent">
-                        <h2 class="content_detail_recommendedagent_title">あなたにオススメのエージェンシー
-                        </h2>
-                        <div class="content_detail_recommendedagent_bigbox">
-                            <article class="content_detail_recommendedagent_box1">
-                                <div class="content_detail_recommendedagent_box1_img"></div>
-                                <a href="https://posse-ap.com/">
-                                    <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedagent_box1_img"></a>
-
-                            </article>
-                            <article class="content_detail_recommendedagent_box2">
-                                <div class="content_detail_recommendedagent_box2_img"></div>
-                                <a href="https://posse-ap.com/">
-                                    <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedagent_box2_img"></a>
-                            </article>
-                            <article class="content_detail_recommendedagent_box3">
-                                <div class="content_detail_recommendedagent_box3_img"></div>
-                                <a href="https://posse-ap.com/">
-                                    <img src="https://reashu.com/wp-content/uploads/2022/01/1b6d9abbd870d9cc7205edfd07ed96ba.png" alt="" class="content_detail_recommendedagent_box3_img"></a>
-                            </article>
-
-                        </div>
                     </div>
                 </div>
                 <!-- ここからお問い合わとかの右側にある情報 -->
@@ -145,11 +121,12 @@ include dirname(__FILE__) . '/header.php';
                             <div class="content_detail_subcontent_right_serch_information_details" id="content_detail_subcontent_right_serch_information_details">
                                 <div class="content_detail_subcontent_right_serch_information_details_companyname">
 
-                                    <a href="https://google.com" class="company_name">株式会社武田鉄</a>
+                                    <a href="<?= Utils::h($agency->url) ?>" class="company_name"><?= Utils::h($agency->name) ?></a>
                                 </div>
-                                <p class="content_detail_subcontent_right_serch_information_detail">2020年に創業</p>
-                                <p class="content_detail_subcontent_right_serch_information_detail">15000のメンバー</p>
-                                <p class="content_detail_subcontent_right_serch_information_detail">東京都港区表参道Harbors</p>
+                                <p><?= Utils::h($agency->email) ?></p>
+                                <p class="content_detail_subcontent_right_serch_information_detail"><?= Utils::h($agency->tel) ?></p>
+                                <p class="content_detail_subcontent_right_serch_information_detail"><?= Utils::h($agency->url) ?></p>
+                                <p class="content_detail_subcontent_right_serch_information_detail"><?= Utils::h($agency->address) ?></p>
 
                             </div>
                         </div>
@@ -176,14 +153,14 @@ include dirname(__FILE__) . '/header.php';
             </div>
         </div>
         </div>
-      
-    
+
+
         <!-- 一番下に固定しておく後で見るに追加とお問合せ -->
 
     </div>
-        
+
 <!-- <div class="detail_blank"></div> -->
-        
+
 
 </main>
 <?php include dirname(__FILE__) . '/footer.php' ?>
